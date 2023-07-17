3992773ddb9abe9a7903aa5a490531c057063fbb
Farxod <urunovfar@gmail.com>
2023-07-17 12:32:08 +0500

    df

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,15 +3,12 @@ import "@/styles/globals.css"
 import "@/styles/responsive.css"
 
 import Head from "next/head";
-// import Layout from "@/components/layout/index"
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* <Layout> */}
       <Head></Head>
         <Component {...pageProps} />
-      {/* </Layout> */}
       
     </>
   );
