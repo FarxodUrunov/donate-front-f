739e9920b33b8066067ac1086219edd4badf70d9
Farxod <urunovfar@gmail.com>
2023-07-17 14:11:07 +0500

    sdf

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-            {
-                source: '/',
-                destination: '/login',
-                permanent: true,
-            },
-        ]
-    },
+    // async redirects() {
+    //     return [
+    //         {
+    //             source: '/',
+    //             destination: '/',
+    //             permanent: true,
+    //         },
+    //     ]
+    // },
 }
 
 module.exports = nextConfig
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 
 const Home = () => {
+
+  const {pathname} = useRouter()
+console.log(pathname)
   return (
     <div>
       Hello
